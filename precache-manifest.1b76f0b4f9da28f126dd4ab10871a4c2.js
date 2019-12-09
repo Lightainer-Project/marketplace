83895ba89a7bd81cf312766c49f758d5c5d99afa
Niki2k1 <Niki2k1@users.noreply.github.com>
2019-12-09 11:46:08 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.1b76f0b4f9da28f126dd4ab10871a4c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270eec3f20af2a5e515386ca669fcee3",
+    "url": "/marketplace/index.html"
+  },
+  {
+    "revision": "6567a7ba9f42c3d54900",
+    "url": "/marketplace/static/css/2.95afd4a9.chunk.css"
+  },
+  {
+    "revision": "6567a7ba9f42c3d54900",
+    "url": "/marketplace/static/js/2.58e9ef77.chunk.js"
+  },
+  {
+    "revision": "dfa67181ac7a5e21d46f",
+    "url": "/marketplace/static/js/main.527b96b4.chunk.js"
+  },
+  {
+    "revision": "74957f561677e49c65da",
+    "url": "/marketplace/static/js/runtime-main.01a07808.js"
+  }
+]);
